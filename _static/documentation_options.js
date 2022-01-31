e96b2d4a6ffce9c28f3e526e31146b1225f742b1
dfm <dfm@dfm.io>
2022-01-31 13:55:59 +0000

    Deploying to gh-pages from @ exoplanet-dev/case-studies@11fd6de876f6cd16d2b2aa0f0511f02b9f3a98b8 🚀

---
+++ _static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.5.3.dev1+g48bf5d8',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'dirhtml',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '',
+    NAVIGATION_WITH_KEYS: true
+};
